044420b7aa8338e2c9891a90ee433fd8e4d6ad3e
longxy <longxinyu2021@163.com>
2026-02-18 21:26:41 +0800

    [remote-shell] feat: disable terminal input function

--- remote-shell/static/shell-integration.zsh
+++ remote-shell/static/shell-integration.zsh
@@ -1,5 +1,31 @@
 # Remote Shell Integration Script for Zsh
 
+# Disable autosuggestions (plugin)
+ZSH_AUTOSUGGEST_DISABLE=true
+# Unbind autosuggest widgets aggressively if needed
+bindkey '^M' accept-line
+
+# Disable syntax highlighting (plugin)
+ZSH_HIGHLIGHT_HIGHLIGHTERS=()
+
+# Disable completion menu and listings to avoid screen redraws
+# Unset options that trigger menu completion or listing
+unsetopt AUTO_MENU          # Don't show menu completion
+unsetopt MENU_COMPLETE      # Don't automatically insert first match
+unsetopt AUTO_LIST          # Don't list choices on ambiguous completion
+unsetopt LIST_TYPES         # Don't show file types in completion
+unsetopt ALWAYS_LAST_PROMPT # Don't return to last prompt after listing (avoids scroll/redraw)
+
+# Disable Flow Control (Ctrl+S/-Q)
+setopt NO_FLOW_CONTROL
+
+# Disable Beep
+setopt NO_BEEP
+
+# Disable Right Prompt (RPROMPT) to avoid redraws on resize/updates
+RPROMPT=""
+RPS1=""
+
 # Disable the "partial line" indicator (%) to keep logs clean
 setopt no_prompt_sp
 
